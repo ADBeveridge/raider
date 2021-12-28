e1c7fb92f6830f5663063de0e963f68a59994193
Alan Beveridge <91360682+ADBeveridge@users.noreply.github.com>
2021-12-28 13:09:07 -0800

    Changed the application id.

--- src/raider.c
+++ src/raider.c
@@ -106,7 +106,7 @@ Raider *
 raider_new (void)
 {
     return g_object_new (raider_get_type (),
-                         "application-id", "org.gnome.Raider",
+                         "application-id", "com.github.ADBeveridge.Raider",
                          "flags", G_APPLICATION_HANDLES_OPEN,
                          NULL);
 }
