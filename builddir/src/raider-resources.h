53e24fcad53528139f6bd95c2cea73fa6604f677
Alan Beveridge <91360682+ADBeveridge@users.noreply.github.com>
2021-12-29 22:42:31 -0800

    Delete builddir directory

--- builddir/src/raider-resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_raider_H__
-#define __RESOURCE_raider_H__
-
-#include <gio/gio.h>
-
-G_GNUC_INTERNAL GResource *raider_get_resource (void);
-#endif
--- builddir/src/theme/theme-resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_theme_H__
-#define __RESOURCE_theme_H__
-
-#include <gio/gio.h>
-
-G_GNUC_INTERNAL GResource *theme_get_resource (void);
-#endif
