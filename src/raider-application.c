3d36611b5cdab1c8e33c82753219c1a896b2e50d
Alan Beveridge <adbeveridge@protonmail.com>
2022-06-24 14:40:42 -0700

    More changes

--- src/raider-application.c
+++ src/raider-application.c
@@ -22,169 +22,165 @@
 #include <glib/gi18n.h>
 
 struct _RaiderApplication {
-  GtkApplication parent_instance;
+	GtkApplication parent_instance;
 };
 
 G_DEFINE_TYPE(RaiderApplication, raider_application, ADW_TYPE_APPLICATION)
 
 RaiderApplication* raider_application_new(gchar * application_id,
-                                          GApplicationFlags flags)
+					  GApplicationFlags flags)
 {
-  return g_object_new(RAIDER_TYPE_APPLICATION, "application-id", application_id, "flags", flags, NULL);
+	return g_object_new(RAIDER_TYPE_APPLICATION, "application-id", application_id, "flags", flags, NULL);
 }
 
 /* Actions. */
 static void raider_new_window(GSimpleAction *action, GVariant *parameter, gpointer user_data)
 {
-  RaiderApplication *self = RAIDER_APPLICATION(user_data);
-  RaiderWindow *window = g_object_new(RAIDER_TYPE_WINDOW, "application", self, NULL);
+	RaiderWindow *window = g_object_new(RAIDER_TYPE_WINDOW, "application", GTK_APPLICATION(user_data), NULL);
 
-  gtk_window_present(GTK_WINDOW(window));
+	gtk_window_present(GTK_WINDOW(window));
 }
 
 static void raider_application_preferences(GSimpleAction *action, GVariant *parameter, gpointer user_data)
 {
-  RaiderPreferences* preferences = g_object_new(RAIDER_TYPE_PREFERENCES, NULL);
-  gtk_window_present(GTK_WINDOW(preferences));
+	RaiderPreferences* preferences = g_object_new(RAIDER_TYPE_PREFERENCES, "transient-for", gtk_application_get_active_window(GTK_APPLICATION(user_data)), NULL);
+	gtk_window_present(GTK_WINDOW(preferences));
 }
 
-static void on_open_response (GtkDialog *dialog, int response)
+static void on_open_response(GtkDialog *dialog, int response)
 {
-  if (response == GTK_RESPONSE_ACCEPT)
-    {
-      GListModel* list = gtk_file_chooser_get_files(GTK_FILE_CHOOSER(dialog));
+	if (response == GTK_RESPONSE_ACCEPT) {
+		GListModel* list = gtk_file_chooser_get_files(GTK_FILE_CHOOSER(dialog));
 
-      int num = g_list_model_get_n_items(list);
-      int i;
-      for (i = 0; i < num; i++)
-        {
-          gpointer obj = g_list_model_get_item(list, i);
-          g_autoptr(GFile) file = obj;
+		int num = g_list_model_get_n_items(list);
+		int i;
+		for (i = 0; i < num; i++) {
+			gpointer obj = g_list_model_get_item(list, i);
+			g_autoptr(GFile) file = obj;
 
-          raider_window_open(g_file_get_path(file), gtk_window_get_transient_for(GTK_WINDOW(dialog)));
-        }
-    }
+			raider_window_open(g_file_get_path(file), gtk_window_get_transient_for(GTK_WINDOW(dialog)));
+		}
+	}
 
-    gtk_window_destroy (GTK_WINDOW (dialog));
+	gtk_window_destroy(GTK_WINDOW(dialog));
 }
 
 static void raider_application_open_to_window(GSimpleAction *action, GVariant *parameter, gpointer user_data)
 {
-  GtkWindow* window = gtk_application_get_active_window(GTK_APPLICATION(user_data));
+	GtkWindow* window = gtk_application_get_active_window(GTK_APPLICATION(user_data));
 
-  GtkDialog *dialog = GTK_DIALOG(gtk_file_chooser_dialog_new(_("Open File"), GTK_WINDOW(window),
-                                                  GTK_FILE_CHOOSER_ACTION_OPEN, _("Cancel"), GTK_RESPONSE_CANCEL, _("Open"),
-                                                  GTK_RESPONSE_ACCEPT, NULL)); /* Create dialog. */
+	GtkDialog *dialog = GTK_DIALOG(gtk_file_chooser_dialog_new(_("Open File"), GTK_WINDOW(window),
+								   GTK_FILE_CHOOSER_ACTION_OPEN, _("Cancel"), GTK_RESPONSE_CANCEL, _("Open"),
+								   GTK_RESPONSE_ACCEPT, NULL)); /* Create dialog. */
 
-  gtk_file_chooser_set_select_multiple(GTK_FILE_CHOOSER(dialog), TRUE); /* Allow to select many files at once. */
-  gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
+	gtk_file_chooser_set_select_multiple(GTK_FILE_CHOOSER(dialog), TRUE); /* Allow to select many files at once. */
+	gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
 
-  g_signal_connect_swapped (dialog, "response", G_CALLBACK (on_open_response), dialog);
-  gtk_widget_show(GTK_WIDGET(dialog));
+	g_signal_connect_swapped(dialog, "response", G_CALLBACK(on_open_response), dialog);
+	gtk_widget_show(GTK_WIDGET(dialog));
 }
 
 static void raider_application_show_about(GSimpleAction *action, GVariant *parameter, gpointer user_data)
 {
-  RaiderApplication *self = RAIDER_APPLICATION(user_data);
-  GtkWindow *window = NULL;
+	RaiderApplication *self = RAIDER_APPLICATION(user_data);
+	GtkWindow *window = NULL;
 
-  g_return_if_fail(RAIDER_IS_APPLICATION(self));
-  window = gtk_application_get_active_window(GTK_APPLICATION(self));
+	g_return_if_fail(RAIDER_IS_APPLICATION(self));
+	window = gtk_application_get_active_window(GTK_APPLICATION(self));
 
-  const gchar *artists[] =
-  {
-    "noëlle",
-    NULL
-  };
-  const gchar *authors[] =
-  {
-    "Alan Beveridge",
-    NULL
-  };
+	const gchar *artists[] =
+	{
+		"noëlle",
+		NULL
+	};
+	const gchar *authors[] =
+	{
+		"Alan Beveridge",
+		NULL
+	};
 
-  g_autofree gchar *program_name = g_strdup("File Shredder");
+	g_autofree gchar *program_name = g_strdup("File Shredder");
 
-  gtk_show_about_dialog(window,
-                        "transient-for", window,
-                        "modal", TRUE,
-                        "program-name", program_name,
-                        "version", _("0.1.0"),
-                        "comments", _("Securely delete your files"),
-                        "license-type", GTK_LICENSE_GPL_3_0,
-                        "artists", artists,
-                        "authors", authors,
-                        "translator-credits", _("translator-credits"),
-                        "logo-icon-name", "com.github.ADBeveridge.Raider",
-                        NULL);
+	gtk_show_about_dialog(window,
+			      "transient-for", window,
+			      "modal", TRUE,
+			      "program-name", program_name,
+			      "version", _("0.1.0"),
+			      "comments", _("Securely delete your files"),
+			      "license-type", GTK_LICENSE_GPL_3_0,
+			      "artists", artists,
+			      "authors", authors,
+			      "translator-credits", _("translator-credits"),
+			      "logo-icon-name", "com.github.ADBeveridge.Raider",
+			      NULL);
 }
 
 static void raider_application_open(GApplication  *application, GFile **files, gint n_files, const gchar   *hint)
 {
-  RaiderWindow *window = g_object_new(RAIDER_TYPE_WINDOW, "application", application, NULL);
-  gint i;
+	RaiderWindow *window = g_object_new(RAIDER_TYPE_WINDOW, "application", application, NULL);
+	gint i;
 
-  for (i = 0; i < n_files; i++)
-    {
-      gchar *path = g_file_get_path(files[i]);
-      raider_window_open(path, window);   // This adds an entry to the current window.
-    }
+	for (i = 0; i < n_files; i++) {
+		gchar *path = g_file_get_path(files[i]);
+		raider_window_open(path, window); // This adds an entry to the current window.
+	}
 
-  gtk_window_present(GTK_WINDOW(window));
+	gtk_window_present(GTK_WINDOW(window));
 }
 
 
 
 static void raider_application_finalize(GObject *object)
 {
-  G_OBJECT_CLASS(raider_application_parent_class)->finalize(object);
+	G_OBJECT_CLASS(raider_application_parent_class)->finalize(object);
 }
 
 static void raider_application_activate(GApplication *app)
 {
-  GtkWindow *window;
+	GtkWindow *window;
 
-  g_assert(GTK_IS_APPLICATION(app));
+	g_assert(GTK_IS_APPLICATION(app));
 
-  /* Get the current window or create one if necessary. */
-  window = gtk_application_get_active_window(GTK_APPLICATION(app));
-  if (window == NULL)
-    window = g_object_new(RAIDER_TYPE_WINDOW, "application", app, NULL);
+	/* Get the current window or create one if necessary. */
+	window = gtk_application_get_active_window(GTK_APPLICATION(app));
+	if (window == NULL)
+		window = g_object_new(RAIDER_TYPE_WINDOW, "application", app, NULL);
 
-  /* Ask the window manager/compositor to present the window. */
-  gtk_window_present(window);
+	/* Ask the window manager/compositor to present the window. */
+	gtk_window_present(window);
 }
 
 static void raider_application_class_init(RaiderApplicationClass *klass)
 {
-  GObjectClass *object_class = G_OBJECT_CLASS(klass);
-  GApplicationClass *app_class = G_APPLICATION_CLASS(klass);
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+	GApplicationClass *app_class = G_APPLICATION_CLASS(klass);
 
-  object_class->finalize = raider_application_finalize;
-  app_class->activate = raider_application_activate;
-  app_class->open = raider_application_open;
+	object_class->finalize = raider_application_finalize;
+	app_class->activate = raider_application_activate;
+	app_class->open = raider_application_open;
 }
 
 static void raider_application_init(RaiderApplication *self)
 {
-  g_autoptr(GSimpleAction) quit_action = g_simple_action_new("quit", NULL);
-  g_signal_connect_swapped(quit_action, "activate", G_CALLBACK(g_application_quit), self);
-  g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(quit_action));
+	g_autoptr(GSimpleAction) quit_action = g_simple_action_new("quit", NULL);
+	g_signal_connect_swapped(quit_action, "activate", G_CALLBACK(g_application_quit), self);
+	g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(quit_action));
 
-  g_autoptr(GSimpleAction) about_action = g_simple_action_new("about", NULL);
-  g_signal_connect(about_action, "activate", G_CALLBACK(raider_application_show_about), self);
-  g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(about_action));
+	g_autoptr(GSimpleAction) about_action = g_simple_action_new("about", NULL);
+	g_signal_connect(about_action, "activate", G_CALLBACK(raider_application_show_about), self);
+	g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(about_action));
 
-  g_autoptr(GSimpleAction) new_window_action = g_simple_action_new("new_window", NULL);
-  g_signal_connect(new_window_action, "activate", G_CALLBACK(raider_new_window), self);
-  g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(new_window_action));
+	g_autoptr(GSimpleAction) new_window_action = g_simple_action_new("new_window", NULL);
+	g_signal_connect(new_window_action, "activate", G_CALLBACK(raider_new_window), self);
+	g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(new_window_action));
 
-  g_autoptr(GSimpleAction) open_action = g_simple_action_new("open", NULL);
-  g_signal_connect(open_action, "activate", G_CALLBACK(raider_application_open_to_window), self);
-  g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(open_action));
+	g_autoptr(GSimpleAction) open_action = g_simple_action_new("open", NULL);
+	g_signal_connect(open_action, "activate", G_CALLBACK(raider_application_open_to_window), self);
+	g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(open_action));
 
-  g_autoptr(GSimpleAction) preferences_action = g_simple_action_new("preferences", NULL);
-  g_signal_connect(preferences_action, "activate", G_CALLBACK(raider_application_preferences), self);
-  g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(preferences_action));
+	g_autoptr(GSimpleAction) preferences_action = g_simple_action_new("preferences", NULL);
+	g_signal_connect(preferences_action, "activate", G_CALLBACK(raider_application_preferences), self);
+	g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(preferences_action));
 
-  gtk_application_set_accels_for_action(GTK_APPLICATION(self), "app.quit", (const char *[]) { "<primary>q", NULL, });
+	gtk_application_set_accels_for_action(GTK_APPLICATION(self), "app.quit", (const char *[]) { "<primary>q", NULL, });
 }
--- src/raider-preferences.c
+++ src/raider-preferences.c
@@ -1,27 +1,38 @@
 #include <gtk/gtk.h>
+#include <adwaita.h>
 
 #include "raider.h"
 #include "raider-window.h"
 #include "raider-preferences.h"
 
 struct _RaiderPreferences {
-  AdwPreferencesWindow parent;
+	AdwPreferencesWindow parent;
 
-  GtkSwitch* remove_file_switch;
+	GtkSwitch* remove_file_switch;
+    AdwComboRow* remove_method_row;
 };
 
 G_DEFINE_TYPE(RaiderPreferences, raider_preferences, ADW_TYPE_PREFERENCES_WINDOW)
 
 static void raider_preferences_class_init(RaiderPreferencesClass *class)
 {
-  gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class), "/com/github/ADBeveridge/Raider/raider-preferences.ui");
-  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), RaiderPreferences, remove_file_switch);
+	gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class), "/com/github/ADBeveridge/Raider/raider-preferences.ui");
+	gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class), RaiderPreferences, remove_file_switch);
 }
 
 
 static void raider_preferences_init(RaiderPreferences *prefs)
 {
-  gtk_widget_init_template(GTK_WIDGET(prefs));
-}
+	gtk_widget_init_template(GTK_WIDGET(prefs));
 
+    GtkStringList *slist = gtk_string_list_new(NULL);
+    gtk_string_list_append(slist, "Wipesync");
+    gtk_string_list_append(slist, "Wipe");
+    gtk_string_list_append(slist, "Unlink");
+
+
+    remove_method_row = adw_combo_row_new();
+    adw_preferences_row_set_title(ADW_PREFERENCES_ROW(remove_method_row), "Remove Method");
+    adw_combo_row_set_model(ADW_COMBO_ROW(combo), G_LIST_MODEL(slist));
+}
 
