30caf2bfeccc9d9daf0ad4016d9f66b1194f08e4
Alan Beveridge <adgbeveridge@proton.me>
2023-03-23 08:51:35 -0700

    Switch to correct URI launcher for help

--- src/raider-application.c
+++ src/raider-application.c
@@ -79,7 +79,7 @@ static void raider_application_open_to_window(GSimpleAction *action, GVariant *p
     gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
 
     g_signal_connect_swapped(dialog, "response", G_CALLBACK(on_open_response), dialog);
-    gtk_widget_show(GTK_WIDGET(dialog));
+    gtk_widget_set_visible(GTK_WIDGET(dialog), TRUE);
 }
 
 static void raider_application_try_exit (GSimpleAction *action, GVariant *parameter, gpointer user_data)
@@ -172,7 +172,8 @@ static void raider_application_show_about(GSimpleAction *action, GVariant *param
 
 static void raider_application_show_help(GSimpleAction *action, GVariant *parameter, gpointer user_data)
 {
-    gtk_show_uri(gtk_application_get_active_window(GTK_APPLICATION(user_data)), "help:raider", GDK_CURRENT_TIME);
+    GtkUriLauncher* launcher = gtk_uri_launcher_new ("help:raider");
+    gtk_uri_launcher_launch(launcher, gtk_application_get_active_window(GTK_APPLICATION(user_data)), NULL, NULL, NULL);
 }
 
 static void raider_application_open(GApplication *application, GFile **files, gint n_files, const gchar *hint)
