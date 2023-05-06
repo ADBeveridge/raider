18a7313f3797e706c77c95de558b58caf71eb96a
Alan Beveridge <adgbeveridge@proton.me>
2023-05-06 13:36:55 -0700

    Remove directory from blueprint subproject

--- src/raider-file-row.c
+++ src/raider-file-row.c
@@ -192,11 +192,11 @@ void raider_file_row_shredding_abort(gpointer data)
 
 void raider_file_row_set_progress(gpointer data)
 {
-    struct _corrupt_data *corrupt_data = data;
+    /*struct _corrupt_data *corrupt_data = data;
     if (!g_mutex_trylock (&corrupt_data->progress_mutex)) { printf("Conficted\n");return; }
     raider_progress_icon_set_progress(corrupt_data->icon, corrupt_data->progress);
     raider_progress_info_popover_set_progress (corrupt_data->popover, corrupt_data->progress);
-    g_mutex_unlock (&corrupt_data->progress_mutex);
+    g_mutex_unlock (&corrupt_data->progress_mutex);*/
 }
 
 RaiderFileRow *raider_file_row_new(GFile *file)
