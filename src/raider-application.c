8262ca9a8f2711b2c625961eb97f674aa3e33ceb
Alan Beveridge <adgbeveridge@proton.me>
2024-06-29 15:44:17 -0700

    Bump minor version for release 3.0.1

--- src/raider-application.c
+++ src/raider-application.c
@@ -170,7 +170,7 @@ static void raider_application_show_about(GSimpleAction *action, GVariant *param
                          "transient-for", window,
                          "application-name", program_name,
                          "application-icon", "com.github.ADBeveridge.Raider",
-                         "version", "3.0.0",
+                         "version", "3.0.1",
                          "copyright", "© 2024 Alan Beveridge",
                          "issue-url", "https://github.com/ADBeveridge/raider/issues/new",
                          "license-type", GTK_LICENSE_GPL_3_0,
